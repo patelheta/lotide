467ff1156be962902faf45bbd6594b1c4fe4968c
patelheta <patelheta385@gmail.com>
2022-10-24 15:16:00 -0700

    Adding new Test file for eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function (actualArrays, expectedArrays) {
+const eqArrays = function(actualArrays, expectedArrays) {
   if (actualArrays.length !== expectedArrays.length) {
     return false;
   }
@@ -21,6 +13,5 @@ const eqArrays = function (actualArrays, expectedArrays) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
+
