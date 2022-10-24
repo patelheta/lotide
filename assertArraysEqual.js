369c234a351bbade887a2b14b34931b43472d9a8
patelheta <patelheta385@gmail.com>
2022-10-24 15:26:23 -0700

    Adding new Test file for assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,6 @@
-const assertArraysEqual = function (actualArray, expectedArray) {
+const eqArrays = require("./eqArrays");
+
+const assertArraysEqual = function(actualArray, expectedArray) {
   if (eqArrays(actualArray, expectedArray)) {
     console.log(`✅✅✅ Assertion Passed: ${actualArray} === ${expectedArray}`);
   } else {
@@ -6,21 +8,4 @@ const assertArraysEqual = function (actualArray, expectedArray) {
   }
 };
 
-const eqArrays = function (actualArrays, expectedArrays) {
-  if (actualArrays.length !== expectedArrays.length) {
-    return false;
-  }
-  if (actualArrays.length === 0) {
-    return true;
-  }
-  for (let i = 0; i < expectedArrays.length; i++) {
-    if (actualArrays[i] !== expectedArrays[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
